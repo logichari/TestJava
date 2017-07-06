2a24006db73cac21842e40d99fbcc2692f3adc03
Sriram Subramanian <sriram@Sangeethas-MacBook-Pro.local>
2017-07-06 18:58:43 +0530

    First message

---
+++ src/testjava/TestJava.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testjava;
+
+/**
+ *
+ * @author logic
+ */
+public class TestJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hi Iam using Java after a long time........");
+        System.out.println("success.....");
+        
+    }
+}
